caae2e70728e8631757dcc662c6766f4735d1d7f
IsaacNuttall <isaacnuttall@gmail.com>
2023-03-24 19:32:55 -0600

    assessment 4 update

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,5 @@
+let myGoals = ['read a book every week', 'go running for 20 minutes every day', 'compliment at least 3 people every day']
+
 module.exports = {
 
     getCompliment: (req, res) => {
@@ -9,7 +11,7 @@ module.exports = {
       
         res.status(200).send(randomCompliment);
     },
-    
+
     getFortune: (req, res) => {
         const fortunes = ["You will become wealthy very soon", "Watch out for Monkeys", "Be sure to invest in soda companies", "Look within, and you will find yourself",
     "Never forget to be close to your family", "Beware the 30th of April"];
@@ -19,7 +21,39 @@ module.exports = {
         let randomFortune = fortunes[randomIndex];
       
         res.status(200).send(randomFortune);
+    },
+
+    addGoal: (req, res) => {
+        let {goal} = req.body
+
+        console.log(goal)
+
+        myGoals.push(goal)
+
+        res.status(200).send(myGoals)
+    },
+
+    deleteGoal: (req, res) => {
+        let index = req.params.id - 1
+
+        myGoals.splice(index, 1)
+
+        res.status(200).send(myGoals)
+    },
+
+    updateGoal: (req, res) => {
+
+        console.log(req,'Updated Goal')
+
+        let {goal} = req.body
+
+        let index = req.params.id - 1
+
+        myGoals.splice(index, 1, goal)
+
+        res.status(200).send(myGoals)
     }
 
 
-}
+
+} 
--- server/index.js
+++ server/index.js
@@ -7,10 +7,16 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
+const { getCompliment, getFortune, addGoal, deleteGoal, updateGoal } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 
-app.get("/api/compliment", getFortune);
+app.get("/api/fortune", getFortune);
+
+app.post("/api/goal", addGoal);
+
+app.delete('/api/goal/:id', deleteGoal);
+
+app.put('/api/goal/:id', updateGoal);
 
 app.listen(4000, () => console.log("Server running on 4000"));
